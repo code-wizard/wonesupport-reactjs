e1c689e448e67ab86d9c41c2b0c7761159ce9ceb
Uchenna Ibe <uchennaibe20@gmail.com>
2019-12-20 16:25:15 +0530

    responsive chatpage

--- src/components/users-side-panel/users-side-panel.component.js
+++ src/components/users-side-panel/users-side-panel.component.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { WoneSidePanel, WoneInnerHeader } from '../common';
-import { Link } from 'react-router-dom'
+import { Accordion, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faUser } from '@fortawesome/free-solid-svg-icons'
 import './users-side-panel.styles.scss';
 
 
@@ -10,18 +13,48 @@ class UsersSidePanel extends Component {
            <WoneSidePanel>
                <WoneInnerHeader>
                     <button className="wone-users-btn add-users">
-                        <span className="fa fa-user"></span>
-                        All users
+                        <FontAwesomeIcon icon={faUser} />
+                        {" "}<span>All users</span>
                     </button>
-                    <Link className="link-add">
-                        <span className="fa fa-plus"></span>
-                        Add
+                    <Link className="link-add" to="#">
+                        <FontAwesomeIcon icon={faPlus} />
+                        {" "}Add
                     </Link>
                </WoneInnerHeader>
                <div className="wone-links">
                   <div className="links-accordions">
                     <div className="link-accordion">
-                        
+                        <Accordion>
+                            <div className="card-header collapsed">
+                                <Accordion.Toggle eventKey="0">
+                                    Start guide
+                                </Accordion.Toggle>
+                                {/* <span className="expand_caret caret -icon">
+                                    <i className="caret-icon"></i>
+                                </span> */}
+                            </div>
+                            <Accordion.Collapse eventKey="0">
+                                <div className="wrap-linker">
+                                    <div className="wrap-full-size">
+                                      <Link to="#">
+                                        <div className="wone_admin-tag"></div>
+                                      </Link> 
+                                    </div>
+                                    <div className="wrap-full-size"></div>
+                                    <div className="wrap-full-size"></div>
+                                    <div className="wrap-full-size"></div>
+                                    <div className="wrap-full-size"></div>
+                                </div>
+                            </Accordion.Collapse>
+                            <div>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                    Click me!
+                                </Accordion.Toggle>
+                            </div>
+                            <Accordion.Collapse eventKey="1">
+                                <div>Hello! I'm another body</div>
+                            </Accordion.Collapse>
+                        </Accordion>
                     </div>
                   </div>
                </div>
